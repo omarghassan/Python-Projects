87e504f3a8920802554dab37ab57d48fb3928a23
Omar Ghassan <107811704+omarghassan@users.noreply.github.com>
2023-04-05 14:01:03 +0300

    Update Pro6 - Multithreading (With Syncronization).py

--- Parallel Programming/Pro6 - Multithreading (With Syncronization).py
+++ Parallel Programming/Pro6 - Multithreading (With Syncronization).py
@@ -1,1 +1,32 @@
 
+#Multithreading (With Syncronization)
+
+from threading import Lock
+class race_condition:
+  amount = 100
+
+  mutex = Lock()
+
+  def husband(self):
+    for i in range(1000000):
+      self.mutex.acquire()
+      self.amount+= 10
+      self.mutex.release()
+    print("Husband Finished")
+
+  def wife(self):
+    for i in range(1000000):
+      self.mutex.acquire()
+      self.amount-= 10
+      self.mutex.release()
+    print("Wife Finished")
+
+rc = race_condition()
+
+
+Thread(target = rc.husband, args = ()).start()
+Thread(target = rc.wife, args = ()).start()
+
+time.sleep(6)
+
+print("Current Amount = ", rc.amount)
